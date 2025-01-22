8da69c886b86ee43d3e5cf4597c407a38e487ecc
Saurya Gur <saurya.gur@outlook.com>
2025-01-22 22:55:13 +0530

    Created form in ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    res.render('index', {title: 'Express'});
 });
+
+router.post("/", (req, res) => {
+    res.send(req.body)
+})
 
 module.exports = router;
